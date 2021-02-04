007f07676a45a9f9adc0e75b92bbb068c47002cf
andyoung <1218853253@qq.com>
2021-02-04 13:52:10 +0800

    netty demo

--- src/main/java/cn/andanyoung/netty/HelloClientHandler.java
+++ src/main/java/cn/andanyoung/netty/HelloClientHandler.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
+//SimpleChannelInboundHandler
 public class HelloClientHandler extends ChannelInboundHandlerAdapter {
 
     private final String message;
